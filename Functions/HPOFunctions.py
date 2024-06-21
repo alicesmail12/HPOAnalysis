b29a1bfbfdac7afaf8459f1f8522cc4c6b75e36f
alicesmail12 <114085896+alicesmail12@users.noreply.github.com>
2024-06-21 09:12:37 +0100

    Add files via upload

--- Functions/HPOFunctions.py
+++ Functions/HPOFunctions.py
@@ -1,15 +1,23 @@
-from pyhpo import Ontology
-_ = Ontology()
-
 def get_HPO_id(HPOTerm):
+    '''
+    Gets HPO ID from an HPO term
+    '''
     term = Ontology.get_hpo_object(HPOTerm)
     return str(term)[:10] if term else None
 
+
 def get_HPO_term(HPOID):
+    '''
+    Gets HPO term from an HPO ID
+    '''
     HPOTerm = Ontology.get_hpo_object(HPOID)
     return str(HPOTerm)[13:] if HPOTerm else None
 
+
 def get_all_parents(HPOID):
+    '''
+    Iteratively gets all parent terms from an HPO ID
+    '''
     term = Ontology.get_hpo_object(HPOID)
     parents = set()
     for parent in term.parents:
@@ -17,10 +25,18 @@ def get_all_parents(HPOID):
         parents |= get_all_parents(str(parent)[:10])
     return parents
 
+
 def get_frequency(HPOTerms_col):
+    '''
+    Gets number of terms from a string separated by '|'
+    '''
     return len(str(HPOTerms_col).split('|'))
 
+
 def propagate_HPO_IDs(HPOIDs_col):
+    '''
+    Gets all parent terms of a list of HPO IDs, only adding terms if they are not already present
+    '''
     HPOIDs = HPOIDs_col.tolist()
     HPOIDs_longlist = []
     for HPOID_list in HPOIDs:
@@ -35,7 +51,11 @@ def propagate_HPO_IDs(HPOIDs_col):
                     HPOIDs_list.append(phenotype)
     return HPOIDs_longlist
 
+
 def get_HPO_terms(HPOIDs_col):
+    '''
+    Converts string of HPO IDs to string of HPO terms
+    '''
     HPOIDs = HPOIDs_col.str.split('|')
     HPOTerms = []
     for IDs in HPOIDs:
@@ -43,7 +63,11 @@ def get_HPO_terms(HPOIDs_col):
         HPOTerms.append('|'.join(terms))
     return HPOTerms
 
+
 def get_child_terms(term, IDChildrenList, termChildrenList):
+    '''
+    Gets all child terms of a term
+    '''
     for HPO in Ontology.get_hpo_object(term).children:
         IDChildrenList.append(str(HPO)[0:10])
         for HPO2 in HPO.children:
